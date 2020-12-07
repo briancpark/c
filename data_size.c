49ca44dc07753704a998ec58d1304626016ef5db
Brian Park <briancpark@berkeley.edu>
2020-12-07 08:03:24 -0500

    added more examples

---
+++ data_size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of int   : %lu\n", sizeof(int));
+    printf("Size of double: %lu\n", sizeof(double));
+    printf("Size of float : %lu\n", sizeof(float));
+    printf("Size of char  : %lu\n", sizeof(char));
+    printf("Size of long  : %lu\n", sizeof(long));
+}
