7d02ac64417cea4a6db22ac8cc8b5f3ce0eb7c15
Brian Park <briancpark@berkeley.edu>
2020-12-07 08:16:43 -0500

    update README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # c
-The C Programming Languages Example
+The C Programming Language Examples. A playground to exercise my proficiency in C.
+
+## Some tips and tricks 
+
+### Compiling
+Don't just do the regular `gcc program.c` command as that will always compile an executable to `a.out`. Instead, name your executable:
+```
+$ gcc -o program program.c
+```
+and you can easily organize and run your program by `./program`!
+
+### Multithreaded Compiling
+If you have a `Makefile`, speed up compiling by multithreading it:
+```
+$ make -j
+```
+
+### See the Results of Preprocessing
+You can directly view the results of preprocessing before it is inputted into the compiler as a `.i` file. It will also give you the produced assembly `.s` file.
+```
+$ gcc -save-temps -o program program.c
+```
+
+### See the Assembly Code
+This compiler flag will output just the `.s` file.
+```
+$ gcc -Wall -S program.c
+```
---
+++ data_size.s
@@ -0,0 +1,70 @@
+	.file	"data_size.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Size of int   : %lu\n"
+.LC1:
+	.string	"Size of double: %lu\n"
+.LC2:
+	.string	"Size of float : %lu\n"
+.LC3:
+	.string	"Size of char  : %lu\n"
+.LC4:
+	.string	"Size of long  : %lu\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$4, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$8, %esi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$4, %esi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$1, %esi
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$8, %esi
+	leaq	.LC4(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
